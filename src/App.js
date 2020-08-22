232c419cdbf5c87d15a6502346583cbf7887db40
aayushigupta66 <aayushigupta66@gmail.com>
2020-08-22 14:03:20 -0700

    added resume and warning

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import pdf from './resume.pdf';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>sup</code>
+          Sorry for the inconvenience!
+          Website under construction.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className='App-link' href={pdf} target="_blank">View Resume</a>
       </header>
     </div>
   );
