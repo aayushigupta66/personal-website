82deecbe340792686e429eb8a519304fe6cfbd58
aayushigupta66 <aayushigupta66@gmail.com>
2020-08-22 12:54:37 -0700

    Updates

---
+++ precache-manifest.6ed46076764c3af9c48b813b108f7467.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711607cd8a74f7d857edb7c86c63b3c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "726406e76c5d6ecbb8cb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "744435753a6cdabd48f3",
+    "url": "/static/js/2.9b55d0e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b55d0e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "726406e76c5d6ecbb8cb",
+    "url": "/static/js/main.9793f8c7.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
