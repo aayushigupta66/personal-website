73dd111f13fee514a29b5cd038ef5d19d1f1c957
aayushigupta66 <aayushigupta66@gmail.com>
2020-08-29 14:06:54 -0500

    landing page complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import React from 'react';
-import './App.css';
+import React, { useState, setState, Component } from 'react';
+import './main.scss';
 import pdf from './resume.pdf';
 
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Work from './component/Work';
+import About from './component/About';
+
+import Button from '@material-ui/core/Button';
+
 function App() {
+
+  const [page, setPage] = useState('work');
+
+  const returnPage = () => {
+    switch(page) {
+      case 'work':
+        return <Work />;
+      case 'about': 
+        return <About />;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Sorry for the inconvenience!
-          Website under construction.
-        </p>
-        <a className='App-link' href={pdf} target="_blank">View Resume</a>
-      </header>
+    <div className='App'>
+      {/* navbar */}
+      <div className='buttonGroup'>
+        <Button className='aayushiButton'
+          onClick={() => { setPage('work') }}>
+            AAYUSHI GUPTA
+        </Button>
+        <Button className='button'
+          onClick={() => { setPage('work') }}>
+            WORK
+        </Button>
+        <Button className='button'
+          onClick={() => { setPage('about') }}>
+            ABOUT
+        </Button>
+      </div>
+      
+      {returnPage()}
     </div>
+
   );
 }
 
---
+++ src/component/Work.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './Work.scss';
+
+import graphic from '../img/graphic.png';
+import pdf from '../resume.pdf';
+import Button from '@material-ui/core/Button';
+
+class Work extends Component {
+  render() {
+    return (
+      <div>
+        <div className='landing'>
+          <span className='hello'>HELLO, </span>
+          <span className='name'>I'M AAYUSHI GUPTA.</span>
+          <div>
+            <span className='position'>SOFTWARE ENGINEER</span>
+          </div>
+          <div>
+            <span className='intro'>Undergrad student @ UIUC seeking full time job as a frontend developer</span>
+          </div>
+          <div className='resume'>
+            <a className='noLink' href={pdf} target="_blank">
+              <Button variant='outlined' className='resumeButton'>
+                VIEW RESUME
+              </Button>
+            </a>
+          </div>
+        </div>
+        <img className='graphic' src={graphic} />
+      </div>
+    );
+  }
+}
+
+export default Work;
