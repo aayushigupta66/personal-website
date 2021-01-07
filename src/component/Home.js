531db5c63ecc951b4b50bd120792590cb5eb8671
aayushigupta66 <aayushigupta66@gmail.com>
2021-01-07 14:07:45 -0800

    updated resume

--- src/component/Home.js
+++ src/component/Home.js
@@ -19,7 +19,7 @@ class Work extends Component {
             <span className='position'>SOFTWARE ENGINEER</span>
           </div>
           <div>
-            <span className='intro'>Undergrad student @ UIUC seeking full time job as a frontend developer</span>
+            <span className='intro'>Undergrad student @ UIUC seeking a full time job as a frontend developer</span>
           </div>
           <div className='resume'>
             <a className='noLink' href={pdf} target="_blank">
